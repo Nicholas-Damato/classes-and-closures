a2daa67694452581c59c9b4235adb4738851a1b7
Nicholas D'Amato <Nicholasvdamato@gmail.com>
2021-04-19 13:31:56 -0600

    don't know how to increment 1 for every 50, everything else done

--- closures.js
+++ closures.js
@@ -22,13 +22,13 @@ function outer() {
   Invoke outer saving the return value into another variable called 'inner'.
 */
   
-// Code Here
+const inner = outer()
 
 
 
 //Once you do that, invoke inner.
 
-//Code Here
+inner()
 
 
 
@@ -51,7 +51,7 @@ function callFriend(name) {
   (HINT: You will need to pass in arguments to both function invocations)
 */
 
-//Code Here
+const callJake = callFriend('Jake', '435-555-9248');
 
 
 
@@ -61,16 +61,24 @@ function callFriend(name) {
   Write a function called makeCounter that makes the following code work properly.
 */
 
-//Code Here
+function makeCounter(){
+  var counter = 0;
+   function addOne(){
+     counter++;
+     return counter
+     
+}
+  return addOne
+}
 
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -186,11 +194,14 @@ function secretNumber() {
   Fix the code below to log the desired output.
 */
 
-function timeOutCounter() {
-  for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
-      console.log(i);
-    }, i * 1000);
+function timeOutCounter(){
+  for (let i = 0; i <= 5; i++){
+    function inner(j){
+    setTimeout(function(){
+      console.log(j)
+      }, j * 1000)
+    }
+  inner(i) 
   }
 }
 timeOutCounter();
